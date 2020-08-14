c72034e64412ffa03b1583fff2851a24dfbc23bb
usagiowo812 <48749706+usagiowo812@users.noreply.github.com>
2020-08-14 11:43:39 +0800

    usagi stitch
    usagi stitch

--- script.js
+++ script.js
@@ -1,43 +1,11 @@
-// 透過getElementById得到的input就是物件
-// 所以可以用.value得到值
-var input = document.getElementById("text");
-var button = document.getElementById("send");
-var list = document.getElementById("list");
-
-// addEventLister 事件監聽 例如click 點擊以後要執行addData
-// 等於在HTML的button 寫 onclick="addData()"
-button.addEventListener("click", addData);
-
-var todoList = [];
-
-function addData() {
-  // alert(input.value);
-  todoList.push(input.value);
-  // alert(todoList)
-  render();
-}
-
-function delData(index) {
-  var temp = [];
-  for (var i = 0; i < todoList.length; i++) {
-    if (i != index) {
-      temp.push(todoList[i]);
+function check(formObj) {
+  var obj = formObj.peas;
+  var selected = [];
+  for (var i = 0; i < obj.length; i++) {
+    if (obj[i].checked) {
+      selected.push(obj[i].value);
     }
   }
-  // alert(temp);
-  todoList = temp;
-  render();
-}
-//   todolist =temp 寫回去
-
-// 刪除array中間特定值的行為
-
-function render() {
-  var listContent = "";
-  for (var i = 0; i < todoList.length; i++) {
-    listContent += `<button onclick="delData(${i})">刪除</button><div>${
-      i + 1
-    } . ${todoList[i]}</div>`;
-  }
-  list.innerHTML = listContent;
+  document.getElementById("buyingList").innerHTML =
+    "您選擇的顏色代碼是 : " + selected.join();
 }
